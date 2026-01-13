0d4179eeef7b4bf336dd644b6fafc6993d6db973
Andreas Abel <andreas.abel@ifi.lmu.de>
2026-01-13 20:59:58 +0100

    Revert the changes done to TestDryRun.hs in commit 4d4659fc98d429452159c8519b37acaf8f112b48.

--- test/TestDryRun.hs
+++ test/TestDryRun.hs
@@ -2,17 +2,11 @@ module TestDryRun (dryRunTests) where
 
 import Test.Tasty
 import Test.Tasty.Golden
+import System.Process (readProcess)
 import System.Directory (setCurrentDirectory, getCurrentDirectory)
 import Data.ByteString.Lazy qualified as BSL
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
-import Text.Printf (printf)
-
--- Import from library
-import StackYaml (findStackYamlFiles, getSymlinkMap)
-import Analysis (analyzeStackYaml)
-import CSV (loadSnapshotDB, ensureCSVFiles)
-import Types (Action(..))
 
 dryRunTests :: [TestTree]
 dryRunTests =
@@ -24,37 +18,9 @@ dryRunTests =
 
 runDryRunTest :: IO BSL.ByteString
 runDryRunTest = do
-  -- Ensure CSV files are available
-  ensureCSVFiles
-
   cwd <- getCurrentDirectory
   setCurrentDirectory "test/tests"
-
-  -- Use library functions to generate dry-run output
-  stackYamlFiles <- findStackYamlFiles
-  db <- loadSnapshotDB
-  symlinkMap <- getSymlinkMap stackYamlFiles
-
-  -- Analyze each file and collect actions
-  actions <- mapM (analyzeFile db symlinkMap) stackYamlFiles
-
-  -- Format output like the real dry-run command
-  let output = unlines $ map formatAction $ concat actions
-
+  output <- readProcess "stacker" ["dry-run", "--color=never"] ""
   setCurrentDirectory cwd
   -- Properly encode as UTF-8
   return $ BSL.fromStrict $ TE.encodeUtf8 $ T.pack output
-  where
-    analyzeFile db symlinkMap file = do
-      maybeAction <- analyzeStackYaml db symlinkMap file
-      return $ maybe [] (:[]) maybeAction
-
-    formatAction action =
-      let file = actionFile action
-          oldSnap = actionOldSnapshot action
-          symlinkMsg = case actionSymlinkTarget action of
-            Just target -> printf "%-20s %-25s = symlink to %s" file (T.unpack oldSnap) target
-            Nothing -> case actionNewSnapshot action of
-              Just newSnap -> printf "%-20s %-25s → bump to %s" file (T.unpack oldSnap) (T.unpack newSnap)
-              Nothing -> printf "%-20s %-25s ✓ up to date" file (T.unpack oldSnap)
-      in symlinkMsg
